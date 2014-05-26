ad800800699ba75d28ca1b90eab72852f12ac7b6
Dr4g0 <d_d0003@yahoo.com>
2014-05-26 22:50:09 +0300

    Put main files back

---
+++ HangmanSix.cs
@@ -0,0 +1,99 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Proekt_KPK
+{
+    class HangmanSix
+    {
+		// hahaha, izpih edno kilo rakiya vcera i poznavam veche vsichki dumi4ki ot pyrvi puyt, muahahahahahahaaaaaaaa
+
+		// test plmb123
+
+        // test plmb123 .gitignore in project folder
+
+        // test Dr4g0
+
+
+        static void Main(string[] args)
+        {
+            string[] words = 
+            {
+                "computer", "programmer", "software", "debugger", "compiler", "developer", "algorithm", 
+                "array", "method", "variable"
+            };
+
+            int numberOfRevealed = 0; // changed l to numberOfRevealed
+            int health = 5; // changed m to health
+            string word = RandomizeAWord(words);
+            string dashWord = new String('-', word.Length);
+            while (numberOfRevealed < word.Length && health > 0)
+            {
+                string input = " ";
+                bool correctInput = true; // changed incorrectInput = false to correctInput = true
+                Console.WriteLine("The word to be guessed is:{0}", dashWord);
+                int letter = 0;
+
+                while ((letter < 'a' || letter > 'z') && (letter < 'A' || letter > 'Z'))
+                {
+                    if (!correctInput)
+                    {
+                        Console.WriteLine("You've entered incorrect input!");
+                    }
+                      
+                    Console.Write("Input a letter:");
+                    input = Console.ReadLine();
+                    letter = input[0];
+                    if ((letter >= 'A' && letter <= 'Z') && letter != 0)
+                    {
+                        letter += 32;
+                    }
+                    correctInput = true;
+                }
+                  
+                correctInput = false;
+                bool isMatch = false;
+
+                char[] tempArr = dashWord.ToCharArray();
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (letter == word[i])
+                    {
+                        tempArr[i] = word[i];
+                        isMatch = true;
+                          
+                        numberOfRevealed++;
+                    }
+                }
+
+                dashWord = new string(tempArr);
+
+                if (isMatch)
+                { Console.WriteLine("Good job! You revealed {0} of the letters and your remaining errors is:{1}", numberOfRevealed, health);
+                }
+                else
+                {
+                    health--;
+                    Console.WriteLine("Sorry there are no unrevealed letters \"{0}\"). Your health is now {1}", (char)letter, health);
+                }
+            }
+
+            if (health == 0)
+            {
+                Console.WriteLine("You lost the game. Try again.");
+            }
+            else
+            {
+                Console.WriteLine("You guessed the word \"{0}\" and you won. Congratulations!", word);
+            }
+        }
+
+        static string RandomizeAWord(string[] arr)
+        {
+            Random rand = new Random();
+
+            int randomNumber = rand.Next(0, arr.Length);
+
+            return arr[randomNumber];
+        }   
+    }
+}
