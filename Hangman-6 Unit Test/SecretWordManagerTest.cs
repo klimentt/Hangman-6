5930cfed61c0e9f7d23fcadfb392adf0edfffe5b
KlimentTodorov <kliment.todorov@euroccor.com>
2014-05-27 21:47:13 +0300

    Added Unit Testing for SecretWordManager

---
+++ Hangman-6 Unit Test/SecretWordManagerTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hangman_6
+{
+    [TestClass]
+    public class SecretWordManager_Test
+    {
+        [TestMethod]
+        public void Loading_Word_Library()
+        {
+            List<string> expected = new List<string>{"computer","programmer","software","debugger","compiler","developer"};
+
+            HangmanSix.SecretWordManager actual = new HangmanSix.SecretWordManager();
+            actual.LoadAllSecretWords(@"../../Test Resources/SecretWordLibraryTest.txt");
+
+            Assert.AreEqual(expected.ToString(), actual.AllSecretWords.ToString());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void Loading_Word_Library_File_Not_Found()
+        {
+            HangmanSix.SecretWordManager invalidPath = new HangmanSix.SecretWordManager();
+            invalidPath.LoadAllSecretWords(@"../../Test Resources/WordLibraryTest.txt");
+        }
+    }
+}
