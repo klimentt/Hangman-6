2b768919bf5ef80794717a0cc0ce8b2c5e18cbc7
KlimentTodorov <kliment.todorov@euroccor.com>
2014-05-29 19:05:32 +0300

    Added ScoreBoard Load

--- ScoreBoard.cs
+++ ScoreBoard.cs
@@ -1,10 +1,30 @@
 ﻿namespace HangmanSix
 {
     using System;
+    using System.IO;
+    using System.Collections.Generic;
     /// <summary>
     /// Keeps data for the top scores
     /// </summary>
     public class ScoreBoard
     {
+        private SortedDictionary<int, string> scoreBoard = new SortedDictionary<int, string>();
+        
+        public SortedDictionary<int, string> ScoreBoard
+        {
+            get { return this.scoreBoard; }
+        }
+        
+        public void Load(string sourceFile)
+        {
+            string[] scoreTemp;
+            string[] scores = File.ReadAllLines(sourceFile);
+            
+            foreach(string score in scores)
+            {
+                scoreTemp = score.Split(',');
+                scoreBoard.Add(int.Parse(scoreTemp[0]),scoreTemp[1]);
+            }
+        }
     }
 }
