6975d8e59628e3de6dcf4a47cfd4b8f654e6b808
KlimentTodorov <kliment.todorov@euroccor.com>
2014-05-29 21:38:28 +0300

    Adding ScoreBoardClass Test

---
+++ Hangman-6 Unit Test/ScoreBoardClass_Test.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hangman_6
+{
+    [TestClass]
+    public class ScoreBoard_Test
+    {
+
+        [TestMethod]
+        public void Loading_File_Test()
+        {
+            HangmanSix.ScoreBoard expected = new HangmanSix.ScoreBoard();
+
+            expected.AddScore(new HangmanSix.Player() { Name = "Milena", Score = 5 });
+            expected.AddScore(new HangmanSix.Player() { Name = "Ivan", Score = 4 });
+            expected.AddScore(new HangmanSix.Player() { Name = "Stancho", Score = 3 });
+            expected.AddScore(new HangmanSix.Player() { Name = "Iva", Score = 2 });
+            expected.AddScore(new HangmanSix.Player() { Name = "Mitko", Score = 1 });
+
+            HangmanSix.ScoreBoard actual = new HangmanSix.ScoreBoard();
+
+            actual.Load(@"../../Test Resources/ScoreBoardTest.txt");
+
+
+            Assert.AreEqual(expected.TopScores.ToString(), actual.TopScores.ToString());
+        }
+
+            [TestMethod]
+            [ExpectedException(typeof(FileNotFoundException))]
+            public void Loading_Files_Exception()
+            {
+                HangmanSix.ScoreBoard testboard = new HangmanSix.ScoreBoard();
+
+                testboard.Load(@"../../Test Resources/ScoreBoardTST.txt");
+            }
+
+            [TestMethod]
+            [ExpectedException(typeof(FileNotFoundException))]
+            public void Savong_File_Exception()
+            {
+                HangmanSix.ScoreBoard testboard = new HangmanSix.ScoreBoard();
+
+                testboard.AddScore(new HangmanSix.Player() { Name = "Milena", Score = 5 });
+                testboard.AddScore(new HangmanSix.Player() { Name = "Ivan", Score = 4 });
+                testboard.AddScore(new HangmanSix.Player() { Name = "Stancho", Score = 3 });
+
+                testboard.Save();
+            }
+        }
+    }
+
+
