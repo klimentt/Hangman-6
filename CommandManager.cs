29f79177e2791d9eb44b2534e80cd8678e9474d2
KlimentTodorov <kliment.todorov@euroccor.com>
2014-06-04 20:25:59 +0300

    Created enumeration for all user commands

--- CommandManager.cs
+++ CommandManager.cs
@@ -6,28 +6,8 @@
     /// <summary>
     /// Keeps all possible commands given from player
     /// </summary>
-    public class CommandManager : IRemovable, IExpandable
+    public enum CommandManager
     {
-        private List<string> allCommands = new List<string>();
-
-        public List<string> AllComands
-        {
-            get { return this.allCommands; }
-        }
-
-        public void LoadAllCommands()
-        { 
-            //read from file and add to allCommands
-        }
-
-        public void Add(string newCommand)
-        {
-            //write to file and add to allCommands
-        }
-
-        public void Remove(int index)
-        {
-            //delete from file and from allComands
-        }
+        Help = "help", EndGame = "exit", TopScore = "top", RestartGame = "restart"
     }
 }
